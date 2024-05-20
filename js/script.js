5eb7f10e3ff547bc823587ec2353e9a853e0da50
”Sn1peRzGG” <afly367@gmail.com>
2024-05-20 11:46:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+let response = await fetch('./data/bands.json')
+let data = await response.json()
+
+const container = document.querySelector('.container')
+const participantsExceptSoloists = []
+
+data.forEach((band, index) => {
+	const bandDiv = document.createElement('div')
+	bandDiv.classList.add('band')
+
+	if (index % 2 === 0) {
+		bandDiv.classList.add('even')
+	} else {
+		bandDiv.classList.add('odd')
+	}
+
+	const participantsWithoutSoloist = band.participants.filter(
+		participant => participant !== band.soloist
+	)
+	participantsExceptSoloists.push(...participantsWithoutSoloist)
+
+	let tracksList = ''
+	band.tracks.forEach(track => {
+		if (track.name && track.duration) {
+			const minutes = (track.duration - (track.duration % 60)) / 60
+			const seconds = track.duration % 60
+			tracksList += `<li>${track.name} - ${minutes} min ${seconds} sec</li>`
+		} else {
+		}
+	})
+
+	bandDiv.innerHTML = `
+		<div class="img-wrapper">
+			<img src="${band.icon}" alt="${band.bandName} Icon" />
+		</div>
+
+		<div class="title-wrapper">
+			<h2>${band.bandName}</h2>
+		</div>
+
+		<div class="soloist-name-wrapper"><h2>${band.soloist}</h2></div>
+
+		<div class="participants-except-soloists-wrapper">
+			<p>${participantsWithoutSoloist.join(', ')}</p>
+		</div>
+
+		<div class="tracks-list-wrapper">
+			<ul class="tracks-list">
+				${tracksList}
+			</ul>
+		</div>`
+
+	container.appendChild(bandDiv)
+})
